5c6edf21ced497bb24d99b9e530af7895c886603
Khushal namdev <144448503+Khushalnama@users.noreply.github.com>
2025-04-30 19:08:03 +0530

    owner responsive

--- src/Pages/owner.jsx
+++ src/Pages/owner.jsx
@@ -15,7 +15,7 @@ const Owner = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-50">
       <h1 className="text-3xl font-bold mb-8">Register Yourself</h1>
-      <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-6">
+      <div className="space-x-6">
         <button
           onClick={handleStudentRegisterClick}
           className="px-6 py-3 cursor-pointer bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 text-white font-semibold rounded-lg shadow-lg hover:from-purple-600 hover:via-pink-600 hover:to-red-600 transition duration-300"
